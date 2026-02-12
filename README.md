09f1434a75f4cb4edfb1e64c61f494b258e705df
eejhay <106743330+eejhay@users.noreply.github.com>
2026-02-12 17:20:57 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Map-Alibut
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Suba-Edel-Jan-M/pen/azZXZqR](https://codepen.io/Suba-Edel-Jan-M/pen/azZXZqR).
+
---
+++ script.js
@@ -0,0 +1,6 @@
+// Function to handle the Map Button
+document.getElementById('mapBtn').addEventListener('click', function() {
+    window.open('https://felt.com/map/Getting-started-with-Felt-9BnL1iN2zTCWzBfQYL70lTA', '_blank');
+});
+
+console.log("Map-Alibut system initialized!");
